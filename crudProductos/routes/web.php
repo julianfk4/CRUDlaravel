c6f597823100be7ded26e46abbf4f99288df694e
juliangamez <julianxdmast@gmail.com>
2025-02-27 11:41:56 +0100

    r publ priv

--- crudProductos/routes/web.php
+++ crudProductos/routes/web.php
@@ -7,26 +7,23 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
+// R públicas
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('login', [App\Http\Controllers\AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [App\Http\Controllers\AuthController::class, 'login']);
 
-Route::get('dashboard/{id}', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
-
-Route::get('crear/{id}', [App\Http\Controllers\ProductosController::class, 'crear'])->name('crear');
-
-Route::post('guardar', [App\Http\Controllers\ProductosController::class, 'guardar'])->name('guardar');
-
-Route::get('detail/{id}', [App\Http\Controllers\ProductosController::class, 'index'])->name('index');
-
-Route::get('edit/{id}', [App\Http\Controllers\ProductosController::class, 'editar'])->name('editar');
-
-Route::get('borrar/{id}', [App\Http\Controllers\ProductosController::class, 'borrar'])->name('borrar');
-
-Route::post('guardarc', [App\Http\Controllers\PostController::class, 'guardarc'])->name('guardarc');
-
-Route::get('borrarc/{id}', [App\Http\Controllers\PostController::class, 'borrarc'])->name('borrarc');
+// R privadas 
+Route::middleware(['auth'])->group(function () {
+    Route::get('dashboard/{id}', [App\Http\Controllers\DashboardController::class, 'dashboard'])->name('dashboard');
+    
+    Route::get('crear/{id}', [App\Http\Controllers\ProductosController::class, 'crear'])->name('crear');
+    Route::post('guardar', [App\Http\Controllers\ProductosController::class, 'guardar'])->name('guardar');
+    
+    Route::get('detail/{id}', [App\Http\Controllers\ProductosController::class, 'index'])->name('index');
+    Route::get('edit/{id}', [App\Http\Controllers\ProductosController::class, 'editar'])->name('editar');
+    Route::get('borrar/{id}', [App\Http\Controllers\ProductosController::class, 'borrar'])->name('borrar');
+    
+    Route::post('guardarc', [App\Http\Controllers\PostController::class, 'guardarc'])->name('guardarc');
+    Route::get('borrarc/{id}', [App\Http\Controllers\PostController::class, 'borrarc'])->name('borrarc');
+});
